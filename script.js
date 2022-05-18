fa238eae098eb4547d8eb58918f506c2edb4fe17
Tapan Das <tapands.das@gmail.com>
2022-05-18 15:21:14 +0530

    Create input from user

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const numberButtons = document.querySelectorAll('.numbers');
 const displayDiv = document.getElementById('display');
+const operatorButtons = document.querySelectorAll('.arithmatic');
 let count = 0;
+
 console.log(numberButtons);
 console.log(displayDiv);
+console.log(operatorButtons);
 
 numberButtons.forEach(number => number.addEventListener('click', function(event) {
     /* monkey patch to clear the dummy display text */
@@ -11,6 +14,19 @@ numberButtons.forEach(number => number.addEventListener('click', function(event)
     } count++; 
     display(event);
 }));
+
+operatorButtons.forEach(operator => operator.addEventListener('click', function(event) {
+    displayDiv.textContent += event.target.innerText;
+    console.log(displayDiv.textContent);
+    makeNumber(displayDiv.textContent);
+}));
+
+function makeNumber(string) {
+    let firstNum = Number(string.slice(0, string.length-1)); 
+    console.log(firstNum, typeof firstNum);
+    let operator = string.slice(string.length-1);
+    console.log(operator,typeof operator);
+};
 
 function display(event) {
     displayDiv.textContent += event.target.innerText;
