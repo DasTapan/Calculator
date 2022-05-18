8a6a6554316c17efb4d662e8b0d2c77057eaefb9
Tapan Das <tapands.das@gmail.com>
2022-05-18 15:08:20 +0530

    Display input numbers

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const numberButtons = document.querySelectorAll('.numbers');
+const displayDiv = document.getElementById('display');
+let count = 0;
+console.log(numberButtons);
+console.log(displayDiv);
+
+numberButtons.forEach(number => number.addEventListener('click', function(event) {
+    /* monkey patch to clear the dummy display text */
+    if(count == 0) {
+        displayDiv.textContent = '';
+    } count++; 
+    display(event);
+}));
+
+function display(event) {
+    displayDiv.textContent += event.target.innerText;
+};
+
 function operate(firstNum, operator, secondNum) {
     switch (operator) {
         case '+':
