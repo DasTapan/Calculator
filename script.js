d1af315590d1d1b8077d32ca3048aa113cb53aac
Tapan Das <tapands.das@gmail.com>
2022-05-19 16:17:01 +0530

    Evaluate multiple pair of numbers

--- script.js
+++ script.js
@@ -10,48 +10,62 @@ let inputArray = [];
 // console.log(displayDiv);
 // console.log(operatorButtons);
 
-numberButtons.forEach(number => number.addEventListener('click', function(event) {
+numberButtons.forEach(number => number.addEventListener('click', function (event) {
     /* monkey patch to clear the dummy display text */
-    if(count == 0) {
+    if (count == 0) {
         displayDiv.textContent = '';
-    } count++; 
-    display(event);
-}));
-
-operatorButtons.forEach(operator => operator.addEventListener('click', function(event) {
+    } count++;
     displayDiv.textContent += event.target.innerText;
-    console.log(displayDiv.textContent);
-    makeNumber(displayDiv.textContent);
+    console.log(`display:${displayDiv.textContent}`);
 }));
 
-result.addEventListener('click', function(event) {
-    let secondNum = Number(displayDiv.textContent);
-    console.log(secondNum, typeof secondNum);
-    inputArray.push(secondNum);
+operatorButtons.forEach(operator => operator.addEventListener('click', function (event) {
+    displayDiv.textContent += event.target.innerText;
+    console.log(`withOperator: ${displayDiv.textContent}`);
+    makeNumber(displayDiv.textContent);
+    //check for existing pair of numbers
+    if (inputArray.length == 3) {
+        let ans = operate(inputArray);
+        displayDiv.textContent = ans;
+        inputArray = [];
+        inputArray.push(ans);
+        inputArray.push(operator.innerText.charCodeAt());
+        count = 0;
+    }
     console.table(inputArray);
+}));
+
+result.addEventListener('click', function (event) {
+    let secondNum = Number(displayDiv.textContent);
+    inputArray.push(secondNum);
     let answer = operate(inputArray);
-    console.log(answer);
     displayDiv.textContent = answer;
     inputArray = [];
+    console.log(`array: ${inputArray}`);
 })
 
-acButton.addEventListener('click',() => {
+acButton.addEventListener('click', () => {
     count = 0;
     displayDiv.textContent = '';
 })
 
 function makeNumber(string) {
-    let firstNum = Number(string.slice(0, string.length-1));
-    inputArray.push(firstNum); 
-    let operator = string.slice(string.length-1).charCodeAt();
-    inputArray.push(operator);
     console.table(inputArray);
-    //clean the display
-    displayDiv.textContent = '';
-};
-
-function display(event) {
-    displayDiv.textContent += event.target.innerText;
+    if (inputArray.length == 2) {
+        console.log('aichi');
+        let lastNum = Number(string.slice(0, string.length - 1));
+        inputArray.push(lastNum);
+    } else {
+        console.log('pasichi');
+        let firstNum = Number(string.slice(0, string.length - 1));
+        inputArray.push(firstNum);
+        let operator = string.slice(string.length - 1).charCodeAt();
+        inputArray.push(operator);
+        //clean the display
+        displayDiv.textContent = '';
+    }
+    // console.log(`display: ${displayDiv.textContent}`);
+    console.table(inputArray);
 };
 
 function operate(array) {
@@ -64,12 +78,12 @@ function operate(array) {
         case 8722:
             return array[0] - array[2];
             break;
-        
+
         //ascii code of html entity &divide    
         case 247:
             return array[0] / array[2];
             break;
-        
+
         //ascii code of html entity &times    
         case 215:
             return array[0] * array[2];
